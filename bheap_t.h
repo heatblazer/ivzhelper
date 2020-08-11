a0f32c7b7a1b528bf276c999fdcebc1461bb5fd6
ilian <heatblazer@gmail.com>
2020-08-11 05:26:18 +0300

    formatting

--- bheap_t.h
+++ bheap_t.h
@@ -5,14 +5,14 @@
 #define BHEAP(NS, T, N, INITSIZE)                                               \
     DARRAY(NS, T, N##_arr, INITSIZE)                                            \
     typedef struct NS##_##N##_t {                                               \
-        NS##_##N##_arr_t* array;                                                    \
+        NS##_##N##_arr_t* array;                                                \
     } NS##_##N##_t;                                                             \
                                                                                 \
     static NS##_##N##_t* NS##_##N##_init(void)                                  \
     {                                                                           \
         NS##_##N##_t* bh = (NS##_##N##_t*)calloc(1, sizeof(NS##_##N##_t));      \
         if (bh) {                                                               \
-            bh->array = NS##_##N##_arr_t_init();                                    \
+            bh->array = NS##_##N##_arr_t_init();                                \
             if (!bh->array) return NULL;                                        \
         }                                                                       \
         return bh;                                                              \
--- dynarray_t.h
+++ dynarray_t.h
@@ -3,97 +3,97 @@
 #include <stdlib.h> /* malloc, calloc, realloc */
 
 //in case no initsize is 0 or less we will assert
-#define DARRAY(NS, T, N, INITSIZE)                                      \
-static const char __attribute__((unused))                           \
-            NS##_##N##_sassertsizeless[INITSIZE <=0 ? -1 : 1];             \
-    typedef struct NS##_##N##_t                                            \
-    {                                                               \
-        size_t size, count;                                         \
-        T* pData;                                                   \
-    } NS##_##N##_t;                                                        \
-                                                                    \
-    typedef int (*T##cmpfn)(T, T);                                  \
-                                                                    \
-    static NS##_##N##_t* NS##_##N##_t##_init(void)                                \
-    {                                                               \
-        NS##_##N##_t* pN = (NS##_##N##_t*)malloc(sizeof(NS##_##N##_t));                  \
-        if (!pN) return NULL;                                       \
-        else {                                                      \
-            pN->pData = (T*)calloc(INITSIZE, sizeof(T));            \
-            if (!pN->pData) { free(pN); return NULL; }              \
-            else {                                                  \
-                pN->count = 0;                                      \
-                pN->size = INITSIZE;                                \
-                return pN;                                          \
-                }                                                   \
-            }                                                       \
-    }                                                               \
-                                                                    \
-    static void NS##_##N##_wiffull(NS##_##N##_t* _this)                           \
-    {                                                               \
-        if (!(_this->count < _this->size-1)) {                      \
-        T* t = (T*)realloc(_this->pData,                            \
-                            sizeof(T)* _this->size * 2);            \
-        if (t) {                                                    \
-            _this->pData = t;                                       \
-            _this->size *= 2;                                       \
-            }                                                       \
-        }                                                           \
-    }                                                               \
-                                                                    \
-    static void NS##_##N##_resizeto(NS##_##N##_t* _this, size_t ns)               \
-    {                                                               \
-        if (ns > _this->size-1) {                                   \
-        T* t = (T*)realloc(_this->pData,                            \
-                            sizeof(T)* ns * 2);                     \
-        if (t) {                                                    \
-            _this->pData = t;                                       \
-            _this->size = ns * 2;                                   \
-            }                                                       \
-        }                                                           \
-    }                                                               \
-                                                                    \
-    static void NS##_##N##_add(T item, NS##_##N##_t* _this)                       \
-        {                                                           \
-            NS##_##N##_wiffull(_this);                                     \
-            *(_this->pData+_this->count) = item;                    \
-            _this->count++;                                         \
-        }                                                           \
-                                                                    \
-    static T* NS##_##N##_getat(size_t idx, NS##_##N##_t* _this)                   \
-    {                                                               \
-        if (idx < _this->count)                                     \
-            return &_this->pData[idx];                              \
-        else return NULL;                                           \
-    }                                                               \
-                                                                    \
-    static void NS##_##N##_cleanup(NS##_##N##_t** _this)                          \
-    {                                                               \
-        if (*_this) {                                               \
-            if ((*_this)->pData)                                    \
-                free((*_this)->pData);                              \
-            (*_this)->pData = NULL;                                 \
-            free(*_this);                                           \
-            (*_this) = NULL;                                        \
-        }                                                           \
-    }                                                               \
-                                                                    \
-    static void NS##_##N##_add_at(T item, size_t idx, NS##_##N##_t* _this)        \
-    {                                                               \
-        NS##_##N##_resizeto(_this, idx);                                   \
-        *(_this->pData+idx) = item;                                 \
-        _this->count++;                                             \
-    }                                                               \
-                                                                    \
-    static T* NS##_##N##_find_if                                           \
-        (T item, NS##_##N##_t* _this, T##cmpfn cmp)                        \
-    {                                                               \
-        size_t i;                                                   \
-        for(i=0; i < _this->count; i++) {                           \
-            if (cmp(item, _this->pData[i]))                         \
-                return &_this->pData[i];                            \
-        }                                                           \
-        return NULL;                                                \
+#define DARRAY(NS, T, N, INITSIZE)                                              \
+static const char __attribute__((unused))                                       \
+            NS##_##N##_sassertsizeless[INITSIZE <=0 ? -1 : 1];                  \
+    typedef struct NS##_##N##_t                                                 \
+    {                                                                           \
+        size_t size, count;                                                     \
+        T* pData;                                                               \
+    } NS##_##N##_t;                                                             \
+                                                                                \
+    typedef int (*T##cmpfn)(T, T);                                              \
+                                                                                \
+    static NS##_##N##_t* NS##_##N##_t##_init(void)                              \
+    {                                                                           \
+        NS##_##N##_t* pN = (NS##_##N##_t*)malloc(sizeof(NS##_##N##_t));         \
+        if (!pN) return NULL;                                                   \
+        else {                                                                  \
+            pN->pData = (T*)calloc(INITSIZE, sizeof(T));                        \
+            if (!pN->pData) { free(pN); return NULL; }                          \
+            else {                                                              \
+                pN->count = 0;                                                  \
+                pN->size = INITSIZE;                                            \
+                return pN;                                                      \
+                }                                                               \
+            }                                                                   \
+    }                                                                           \
+                                                                                \
+    static void NS##_##N##_wiffull(NS##_##N##_t* _this)                         \
+    {                                                                           \
+        if (!(_this->count < _this->size-1)) {                                  \
+        T* t = (T*)realloc(_this->pData,                                        \
+                            sizeof(T)* _this->size * 2);                        \
+        if (t) {                                                                \
+            _this->pData = t;                                                   \
+            _this->size *= 2;                                                   \
+            }                                                                   \
+        }                                                                       \
+    }                                                                           \
+                                                                                \
+    static void NS##_##N##_resizeto(NS##_##N##_t* _this, size_t ns)             \
+    {                                                                           \
+        if (ns > _this->size-1) {                                               \
+        T* t = (T*)realloc(_this->pData,                                        \
+                            sizeof(T)* ns * 2);                                 \
+        if (t) {                                                                \
+            _this->pData = t;                                                   \
+            _this->size = ns * 2;                                               \
+            }                                                                   \
+        }                                                                       \
+    }                                                                           \
+                                                                                \
+    static void NS##_##N##_add(T item, NS##_##N##_t* _this)                     \
+        {                                                                       \
+            NS##_##N##_wiffull(_this);                                          \
+            *(_this->pData+_this->count) = item;                                \
+            _this->count++;                                                     \
+        }                                                                       \
+                                                                                \
+    static T* NS##_##N##_getat(size_t idx, NS##_##N##_t* _this)                 \
+    {                                                                           \
+        if (idx < _this->count)                                                 \
+            return &_this->pData[idx];                                          \
+        else return NULL;                                                       \
+    }                                                                           \
+                                                                                \
+    static void NS##_##N##_cleanup(NS##_##N##_t** _this)                        \
+    {                                                                           \
+        if (*_this) {                                                           \
+            if ((*_this)->pData)                                                \
+                free((*_this)->pData);                                          \
+            (*_this)->pData = NULL;                                             \
+            free(*_this);                                                       \
+            (*_this) = NULL;                                                    \
+        }                                                                       \
+    }                                                                           \
+                                                                                \
+    static void NS##_##N##_add_at(T item, size_t idx, NS##_##N##_t* _this)      \
+    {                                                                           \
+        NS##_##N##_resizeto(_this, idx);                                        \
+        *(_this->pData+idx) = item;                                             \
+        _this->count++;                                                         \
+    }                                                                           \
+                                                                                \
+    static T* NS##_##N##_find_if                                                \
+        (T item, NS##_##N##_t* _this, T##cmpfn cmp)                             \
+    {                                                                           \
+        size_t i;                                                               \
+        for(i=0; i < _this->count; i++) {                                       \
+            if (cmp(item, _this->pData[i]))                                     \
+                return &_this->pData[i];                                        \
+        }                                                                       \
+        return NULL;                                                            \
     }
 
 #endif // DYNARRAY_T_H
